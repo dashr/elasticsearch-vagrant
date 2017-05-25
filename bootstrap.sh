64be83cd2c952f469b39b3e08d2c0ee5a1fc0bf3
jose d lopez <jose@devlabs.vc>
2017-05-24 22:09:23 -0700

    upgrade for 16.04, plus use Elastic.CO sources

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,19 @@
 sudo apt-get update
-sudo apt-get install wget openjdk-7-jdk -y
-wget -qO - https://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
 
 # Install python tools
 sudo apt-get install build-essential autoconf flex bison libtool python-dev -y
 
-# Commands taken from
-# http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/setup-repositories.html
-wget https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.0.1/elasticsearch-2.0.1.deb
-sudo dpkg -i elasticsearch-2.0.1.deb
-sudo sed -i  '/network.host:/a network.bind_host: _non_loopback_' /etc/elasticsearch/elasticsearch.yml
-sudo update-rc.d elasticsearch defaults 95 10
-sudo /etc/init.d/elasticsearch start
+# Install direct elasticsearch repo
+sudo apt-get install htop openjdk-8-jre -y
+wget -qO - https://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb https://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
+
+
+# Commands mostly taken from
+# https://www.elastic.co/guide/en/elasticsearch/reference/2.3/setup-repositories.html
+sudo sudo apt-get update & apt-get install elasticsearch -y
+sudo sed -i 's/#START_DAEMON/START_DAEMON/' /etc/default/elasticsearch
+sudo echo "network.bind_host: 0" >> /etc/elasticsearch/elasticsearch.yml
+sudo echo "network.host: 0.0.0.0" >> /etc/elasticsearch/elasticsearch.yml
+sudo update-rc.d elasticsearch defaults
+sudo systemctl restart elasticsearch
